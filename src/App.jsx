ea934e949b07228b2fc8c1871994a1655d1a662a
Raffa <203495392+RaffaHitipeuw@users.noreply.github.com>
2025-09-26 11:37:14 +0700

    Summit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Layout from './components/Page'
+
+
+
+function App() {
+  return (
+    <>
+    <Layout/>
+    <main onClick={() => {}}></main>
+    </>
+  )
+}
+
+export default App
